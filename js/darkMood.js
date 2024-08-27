6172503502362bda0aeca9d25e0a1c522fb52e42
Hadi-AL-Saden <100237201+Hadi-AL-Saden@users.noreply.github.com>
2024-08-27 21:43:24 +0300

    Add files via upload

---
+++ js/darkMood.js
@@ -0,0 +1,9 @@
+const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// if (isDarkMode) {
+//     document.body.style.backgroundColor = "#333"; 
+//     document.body.style.color = "#fff";
+// } else {
+//     document.body.style.backgroundColor = "#fff"; 
+//     document.body.style.color = "#000"; 
+// }
